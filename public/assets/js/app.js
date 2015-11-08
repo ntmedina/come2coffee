ecc2f8ef12e8147b745d713df8c21cdd847d818a
Raphael Amorim <rapha850@gmail.com>
2015-11-08 03:19:38 -0200

    added frontend files

---
+++ public/assets/js/app.js
@@ -0,0 +1,6 @@
+var btnDown      = document.querySelector('.scrollToMap'),
+    sectionPanel = document.querySelector('.panel');
+btnDown.addEventListener('click', function(e) {
+   sectionPanel.classList.add('off');
+
+});
